22356c27b38f5f3a8aff76dfabe7cc45e92cd0d4
sukra <sukra@DESKTOP-OB7AJV3>
2025-02-09 14:42:33 +0300

    Add project files.

---
+++ list2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+public class Program
+{
+    public static void Main()
+    {
+        List<string> kahveler = new List<String>();
+
+        Console.WriteLine("lütfen 5 kahve ismi giriniz");
+        for (int i = 1; i <= 6; i++)
+        {
+            Console.WriteLine($"kahve {i}:");
+            string kahve = Console.ReadLine();
+            kahveler.Add(kahve);
+        }
+        Console.WriteLine("girilen kahveler:");
+        foreach (string kahve in kahveler)
+        {
+            Console.WriteLine(kahve);
+        }
+    }
+}
+
+
+
